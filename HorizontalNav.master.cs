7b3d0b92cd64476892170269ec8dd9df976146be
tamjd1 <taha.amjad@uoit.net>
2014-01-25 00:28:36 -0500

    blah

--- HorizontalNav.master.cs
+++ HorizontalNav.master.cs
@@ -67,7 +67,36 @@ namespace WebApplication1
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (lblNextRace != null)
+            {
+                int nextRace = this.daysUntilNextRace();
 
+                switch (nextRace)
+                {
+                    case 0:
+                        lblNextRace.Text = "Race Day!";
+                        break;
+                    case 1:
+                        lblNextRace.Text = "Next race is tomorrow!";
+                        break;
+                    default:
+                        lblNextRace.Text = String.Format("There are only {0} days left until the next race!", nextRace);
+                        break;
+                }
+            }
+        }
+
+        private int daysUntilNextRace()
+        {
+            DateTime nextRace = new DateTime(DateTime.Today.Year, 5, 4);
+
+            if (DateTime.Today > nextRace)
+            {
+                nextRace = nextRace.AddYears(1);
+            }
+            TimeSpan ts = nextRace - DateTime.Today;
+
+            return ts.Days;
         }
     }
 }
