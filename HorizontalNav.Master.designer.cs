b80d5fd9e4651a90422b137fac67f70f6de4d004
tamjd1 <taha.amjad@uoit.net>
2014-01-31 15:54:59 -0500

    clean up on master pages, deleted unnecessary files/folders

--- HorizontalNav.Master.designer.cs
+++ HorizontalNav.Master.designer.cs
@@ -110,5 +110,14 @@ namespace WebApplication1 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        
+        /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
     }
 }
